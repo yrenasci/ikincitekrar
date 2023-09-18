5d23b5070bcf803ea0852ab94d50154bb9c8b9fd
Yaren Aşcı <127778015+yrenasci@users.noreply.github.com>
2023-09-18 23:50:24 +0300

    Ödev
    ödev

---
+++ bootstap/calisma/anasayfa.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    // Sayfayı yeni bir URL'ye yönlendir
+    window.location.href = "index.html";
+});
---
+++ bootstap/calisma/buton.js
@@ -0,0 +1,4 @@
+document.getElementById("myBUTTON").addEventListener("click", function() {
+    // Sayfayı yeni bir URL'ye yönlendir
+    window.location.href = "hakkimizda.html";
+});
